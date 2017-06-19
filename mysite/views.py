f76fb3d8f385f0cacb3de7d7aee7e1af2f5c0155
Colin Potter <colinpotter@gmail.com>
2017-06-18 21:38:46 -0700

    removed unneeded imports

---
+++ mysite/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponse
+from django.views import generic
+
+# Create your views here.
+def index(request):
+    return HttpResponse('<a href="polls">Polls</a><br><a href="admin">Admin</a>')
